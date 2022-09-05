2d66055e4c53d8e76b82969000fe55ad6dd5cb71
Joshua-123-ansah <owusuja500@gmail.com>
2022-09-05 12:04:35 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import { Button, Paper, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [editor, setEditor] = useState<EditorJS | null>();
+
+  useEffect(() => {
+    const editor = new EditorJS({
+      holder: "editorjs",
+
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: ["link"],
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+      },
+      onReady: () => {
+        setEditor(editor);
+      },
+      autofocus: true,
+      onChange: (api, event) => {
+        editor.save()?.then((data) => {
+          console.log(data);
+        });
+
+        // console.log("Now I know that Editor's content changed!", event);
+      },
+    });
+  }, []);
+
+  const onSave = () => {
+    console.log(editor);
+    editor
+      ?.save()
+      .then((outputData: any) => {
+        // const array = outputData.blocks;
+        console.log("Article data: ", outputData);
+      })
+      .catch((error: any) => {
+        console.log("Saving failed: ", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Paper elevation={3} sx={{ margin: "50px", height: "700px" }}>
+        <div id="editorjs"></div>
+      </Paper>
+      <Typography align="center">
+        <Button variant="contained" color="success" onClick={onSave}>
+          Submit
+        </Button>
+      </Typography>
+    </>
   );
 }
 
